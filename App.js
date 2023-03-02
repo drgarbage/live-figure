f2e77ff0d31a9b1fa869fb4eb3eb94c6167e0bec
Johnson Wang <drgarbage@gmail.com>
2023-03-02 09:50:16 +0800

    add host & options

--- App.js
+++ App.js
@@ -1,33 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, Image, TouchableOpacity, ActivityIndicator, SafeAreaView } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import { img2img } from './src/api';
+import { Slider } from '@miblanchard/react-native-slider';
 import * as ImagePicker from 'expo-image-picker';
 import ImageView from "react-native-image-viewing";
 
 export default function App() {
   const [image, setImage] = useState(null);
   const [result, setResult] = useState(null);
+  const [host, setHost] = useState('https://50cb5f29-178c-4061.gradio.live');
   const [options, setOptions] = useState({
-    denoising_strength: 0.48,
+    denoising_strength: 0.42,
     cfg_scale: 8.5,
+    width: 768,
+    height: 768,
   })
   const [loading, setLoading] = useState(false);
   const [enlarge, setEnlarge] = useState(false);
+
+  const config = async () => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    if(status !== 'granted') return alert("Camera access not allowed.");
+    setHost(null);
+  }
+
+  const barcodeScanned = ({type, data}) => {
+    if(!data.startsWith('https://') || !data.endsWith('.gradio.live')) return;
+    setHost(data);
+  }
 
   const capture = async () => {
     const permission = await ImagePicker.requestCameraPermissionsAsync();
     if(!permission.granted) return alert('Camera access not allowed.');
 
     const captureResult = await ImagePicker.launchCameraAsync({
-      base64: true,
-      allowsEditing: true,
-      aspect: [1,1],
-      quality: 1
+      base64: true
     });
 
     if(captureResult.canceled) return;
-    setImage('data:image/jpeg;base64,' + captureResult.assets[0].base64);
+    const asset = captureResult.assets[0];
+    const rate = Math.min(768 / asset.width, 768 / asset.height);
+    const size = { width: parseInt(asset.width * rate), height: parseInt(asset.height * rate)};
+    setOptions(opt => ({...opt, ...size}));
+    setImage('data:image/jpeg;base64,' + asset.base64);
   }
 
   const pickImage = async () => {
@@ -35,61 +52,100 @@ export default function App() {
     if(!permission.granted) return alert('Image access not allowed.');
 
     const pickResult = await ImagePicker.launchImageLibraryAsync({
-      base64: true,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1
+      base64: true
     });
 
     if(pickResult.canceled) return;
-    setImage('data:image/jpeg;base64,' + pickResult.assets[0].base64);
+    const asset = pickResult.assets[0];
+    const rate = Math.min(768 / asset.width, 768 / asset.height);
+    const size = { width: parseInt(asset.width * rate), height: parseInt(asset.height * rate)};
+    setOptions(opt => ({...opt, ...size}));
+    setImage('data:image/jpeg;base64,' + asset.base64);
   }
 
   const reload = async () => {
     if(!image || image.length == 0) return;
     try{
       setLoading(true);
-      const sdResult = await img2img(image, options);
+      const sdResult = await img2img(image, options, host);
       setResult('data:image/jpeg;base64,' + sdResult);
       setEnlarge(true);
     }catch(err){
       alert(err);
-      console.log(err);
+      console.error(err);
     }finally{
       setLoading(false);
     }
   }
 
-  useEffect(() => { reload() }, [image, options]);
-
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <TouchableOpacity 
-        onPress={()=>setEnlarge(true)}
-        style={styles.image}>
-        <Image 
-          source={{uri:image}} 
-          style={styles.image} 
-          resizeMode='contain'
+
+      {!host &&
+        <BarCodeScanner
+          onBarCodeScanned={barcodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      }
+
+      { !!host && 
+        <TouchableOpacity 
+          onPress={()=>setEnlarge(true)}
+          style={styles.image}>
+          <Image 
+            source={{uri:image}} 
+            style={styles.image} 
+            resizeMode='contain'
+            />
+        </TouchableOpacity>
+      }
+
+      { loading && 
+        <ActivityIndicator 
+          animating={loading} 
+          size="large" 
+          style={styles.loading} 
+          color="white"
           />
-      </TouchableOpacity>
-      <View style={styles.toolbar}>
-        <Button title='Camera' onPress={capture} />
-        <Button title='Photos' onPress={pickImage} />
-        <Button title='Reload' onPress={reload} />
+      }
+      
+      <View style={styles.slider}>
+        <Slider
+          style={styles.fullWidth}
+          value={options.cfg_scale}
+          maximumValue={30}
+          minimumValue={1}
+          step={.5}
+          onValueChange={([cfg_scale]) => setOptions(opt => ({...opt, cfg_scale}))}
+        />
+        <Text style={styles.sliderText}>{options.cfg_scale}</Text>
       </View>
+      <View style={styles.slider}>
+        <Slider
+          style={styles.slider}
+          value={options.denoising_strength}
+          maximumValue={1}
+          minimumValue={0}
+          step={.01}
+          onValueChange={([denoising_strength]) => setOptions(opt => ({...opt, denoising_strength}))}
+        />
+        <Text style={styles.sliderText}>{options.denoising_strength}</Text>
+      </View>
+
+      <View style={styles.toolbar}>
+        <Button title='H' onPress={config} />
+        <Button title='PHOTO' onPress={pickImage} />
+        <Button title='CAM' onPress={capture} />
+        <Button title='RELOAD' onPress={reload} />
+      </View>
+
       <ImageView 
+        doubleTapToZoomEnabled
         visible={enlarge}
         onRequestClose={()=>setEnlarge(false)}
         images={[{uri: result}]} 
         style={{width: 300, height: 300, alignItems: 'center', justifyContent: 'center' }} 
-        />
-      <ActivityIndicator 
-        animating={loading} 
-        size="large" 
-        style={styles.loading} 
-        color="white"
         />
     </SafeAreaView>
   );
@@ -104,6 +160,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   full: { flex: 1 },
+  fullWidth: {flex: 1, width: '100%' },
   image: {
     flex: 1,
     width: '100%',
@@ -120,5 +177,16 @@ const styles = StyleSheet.create({
     bottom: 0,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  slider: {
+      width: '100%',
+      height: 44,
+      paddingLeft: 20,
+      paddingRight: 20,
+      alignItems: 'stretch',
+      justifyContent: 'center',
+  },
+  sliderText: {
+
   }
 });
--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,29 @@
-const HOST = 'https://edd53833-27fd-4f58.gradio.live';
 const prompt =
-  "right hand touching herself, (naked:1.6), (nude:1.2), nipple, pussy, legs open, stand in front of beach, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, wet skin, transparent clothes, see through, (ulzzang:1.2)";
+  "a giant penis is pushing in between teenager girl's legs, which is wet and sticky, and the white liquid was splash all over her body, she was afraid and feel disgusting and helpless, she turn her head away, trying to avoid the incoming penis. shw is crying and screaming, and both of her arm was tied up, her clothes was broken and tearing apart, she is half naked, (nude:1.6), elf, fairy, child, under age, nipple, wet, pubit hair, sweaty, (sticky white liquid on her face:1.4), sharp focus, RAW photo, delicate, best quality, (intricate details:1.3), hyper detail, finely detailed, colorful, 1girl, solo, 8k uhd, film grain, (studio lighting:1.2), (Fujifilm XT3), (photorealistic:1.3), (detailed skin:1.2),1 ,(nose blush),middle breasts,beautiful detailed eyes, daylight, wet, rain, (short hair:1.2),floating hair NovaFrogStyle, full body,turtleneck,ribbed sweater,see-through,wet clothes,(ulzzang:1.2), wet dick, cum";
 const negative_prompt =
   'Negative prompt: extra fingers, paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, age spot, (outdoor:1.6), manboobs, backlight,(ugly:1.331), (duplicate:1.331), (morbid:1.21), (mutilated:1.21), (tranny:1.331), mutated hands, (poorly drawn hands:1.331), blurry, (bad anatomy:1.21), (bad proportions:1.331), extra limbs, (disfigured:1.331), (more than 2 nipples:1.331), (missing arms:1.331), (extra legs:1.331), (fused fingers:1.61051), (too many fingers:1.61051), (unclear eyes:1.331), bad hands, missing fingers, extra digit, (futa:1.1), bad body, NG_DeepNegative_V1_75T,pubic hair, glans, (lines:1.5), (stroke:1.5)';
 const defaultOptions = {
   prompt,
   negative_prompt,
   steps: 24,
-  denoising_strength: 0.48,
-  cfg_scale: 8.5,
+  denoising_strength: 0.4,
+  cfg_scale: 8,
   mask_blur: 4,
   seed: -1,
   batch_size: 1,
-  width: 512,
-  height: 512,
+  width: 768,
+  height: 768,
   sampler_index: 'DPM++ SDE Karras'
 };
 
-export const img2img = async (img, options) => {
+export const img2img = async (img, options, host) => {
   const body = {
     ...defaultOptions,
     ...options,
     init_images: [img],
   };
 
-  const response = await fetch(`${HOST}/sdapi/v1/img2img`, {
+  const response = await fetch(`${host}/sdapi/v1/img2img`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -32,6 +31,9 @@ export const img2img = async (img, options) => {
     },
     body: JSON.stringify(body),
   });
+
+  if(!response.ok)
+    throw new Error('Network Error: ' + response.status);
 
   const result = await response.json();
 
