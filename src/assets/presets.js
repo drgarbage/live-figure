42b1175498e997322d47fc1396a9a61e7a2c60c5
Johnson Wang <drgarbage@gmail.com>
2023-03-20 03:15:27 +0800

    add more presets

--- src/assets/presets.js
+++ src/assets/presets.js
@@ -1,23 +1,31 @@
 export const DARK_PRESETS = [
+  { prompt: "(naked:1.6), nude, nipple, wet, pubit hair, sweaty, sharp focus, RAW photo, delicate, best quality, (intricate details:1.3), hyper detail, finely detailed, colorful, 1girl, solo, 8k uhd, film grain, (studio lighting:1.2), (Fujifilm XT3), (photorealistic:1.3), (detailed skin:1.2),1 ,(nose blush),middle breasts,beautiful detailed eyes, daylight, wet, rain, (short hair:1.2),floating hair NovaFrogStyle, full body" },
   { prompt: "a giant penis is pushing in between teenager girl's legs, which is wet and sticky, and the white liquid was splash all over her body, she was afraid and feel disgusting and helpless, she turn her head away, trying to avoid the incoming penis. shw is crying and screaming, and both of her arm was tied up, her clothes was broken and tearing apart, she is half naked, (nude:1.6), elf, fairy, child, under age, nipple, wet, pubit hair, sweaty, (sticky white liquid on her face:1.4), sharp focus, RAW photo, delicate, best quality, (intricate details:1.3), hyper detail, finely detailed, colorful, 1girl, solo, 8k uhd, film grain, (studio lighting:1.2), (Fujifilm XT3), (photorealistic:1.3), (detailed skin:1.2),1 ,(nose blush),middle breasts,beautiful detailed eyes, daylight, wet, rain, (short hair:1.2),floating hair NovaFrogStyle, full body,turtleneck,ribbed sweater,see-through,wet clothes,(ulzzang:1.2), wet dick, cum" },
 ];
 
 export const PRESETS = {
   COS: [
-    { prompt: "RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body"},
-    { prompt: "RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, ulzzang"},
-    { prompt: "RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, (ulzzang:1.2)"},
+    { prompt: "RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through"},
+    { prompt: "RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through, ulzzang"},
+    { prompt: "RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through, (ulzzang:1.2)"},
+    { prompt: "RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through, <lora:shojovibe:0.6>"},
   ],
-  PINK: [
+  STAR: [
+    { prompt: "Photo of YoonJi, 1girl, cute, smiling, child, under age, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through, <lora:YoonJi:0.65>"},
     { prompt: "Photo of Lisa Blackpink, (smiling:1.5), (black leotard:1.3), black shirt, k-pop Idol, contrapposto, best quality, ultra high res, (photorealistic:1.3), 1girl, small breasts, (in a nighclub:1.3), beauty512, <lora:Lisa Blackpink:0.8>" },
     { prompt: "Photo of Jennie, ((white mini dress)), smile, kpop Idol, contrapposto, best quality, ultra high res, (photorealistic:1.3), 1girl, small breasts, at a nightclub <lora:Jennie Blackpink:0.8>" },
+    { prompt: "Photo of gldot, 1girl, charming, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through, <lora:gldot:0.8>" },
+    { prompt: "Photo of Liyuu, 1girl, charming, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through, <lora:liyuu:0.8>" },
+    { prompt: "Photo of Goutou, _gtll_, <lora:goutouLora_v20:0.9>, (RAW photo:1.2), ((photorealistic:1.4),(masterpiece:1.3),(best quality:1.4),([blue|green] women's dress:1.4), (black hair),ultra high res,(detailed facial features),(detailed clothes features),HDR,8k resolution, bangs, full body,skin tight, (shiny skin), (red eyes:1.2),(cute girl),(cute face), (only 16), dreamlike, (extreme detailed illustration), scenery, no text, very long hair, (small breasts), twin_tail" },
   ],
   WAR: [
-    { prompt: "masterpiece, best quality, ultra-detailed, illustration, 1girl, (holding weapon, aiming at viewer, assault rifle,left view:1.5), carbine, open fire, firing, <lora:HK416-v2:0.6>" },
-    { prompt: "military gears, navy seal, tactical headset, military helmet, ammo pouches, plate carrier, armor vest, masterpiece, best quality, ultra-detailed, illustration, 1girl, (holding weapon, aiming at viewer, assault rifle,left view:1.5), carbine, open fire, firing, <lora:HK416-v2:0.6>" },
+    { prompt: "masterpiece, holding weapon, aiming at viewer, assault rifle, carbine, open fire, firing, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through ulzzang <lora:HK416-v2:0.6>" },
+    { prompt: "military gears, navy seal, tactical headset, military helmet, ammo pouches, plate carrier, armor vest, masterpiece, best quality, ultra-detailed, illustration, 1girl, (holding weapon, aiming at viewer, assault rifle,left view:1.5), carbine, open fire, firing, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through ulzzang <lora:HK416-v2:0.6>" },
   ],
   MOTOR: [
     { prompt: "Portrait of motorcycle rider, a beautiful female model, georgia fowler, beautiful face, with short dark brown hair, in cyberpunk city at night. She is wearing a leather jacket, black jeans, dramatic lighting, (police badge:1.2)" },
     { prompt: "Motorcycle rider, blck bra, leather pants, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, ulzzang" },
+    { prompt: "Portrait of motorcycle rider, a beautiful female model, georgia fowler, beautiful face, with short dark brown hair, in cyberpunk city at night. She is wearing a leather jacket, black jeans, dramatic lighting, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through (police badge:1.2)" },
+    { prompt: "Motorcycle rider, blck bra, leather pants, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, RAW photo, best quality, ultra high res, photoshoot, (photorealistic:1.4), 1girl, singlet, sleeveless, (light brown hair:1), looking at viewer, smiling, cute, full body, wet clothes, rain, see-through ulzzang" },
   ]
 }
